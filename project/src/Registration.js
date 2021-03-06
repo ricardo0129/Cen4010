ef604fa6985ce5204eeac954b68aa5ec2d6a9cf9
dalonso31 <alonsod2000@gmail.com>
2021-03-06 16:34:01 -0500

    nagivation stack set up

---
+++ project/src/Registration.js
@@ -0,0 +1,21 @@
+import { registerRootComponent} from 'expo';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Button,
+  TouchableOpacity,
+} from "react-native";
+
+const Registration= () => {
+return(
+  <Text>Registration Placeholder</Text>
+)
+}
+
+
+export default Registration;
